39f143d0d193286a9c34c0bc3632d5a8c4cba56e
Henrietta Odiete <henriettaodiete@v1020-wn-162-186.campus-dynamic.uwaterloo.ca>
2015-01-29 20:16:44 -0500

    Initial Commit

---
+++ Project1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project1
+//
+//  Created by Henrietta Odiete on 1/29/15.
+//  Copyright (c) 2015 Henrietta Odiete. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
